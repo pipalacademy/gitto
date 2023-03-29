859ac92a534ab54e9bf52946fec5f8fd173a2d38
Anand Chitipothu <anandology@gmail.com>
2023-03-29 14:43:03 +0530

    gofmt + goimports

--- repo.go
+++ repo.go
@@ -1,216 +1,217 @@
 package main
 
 import (
-    "bytes"
-    "encoding/json"
-    "errors"
-    "io"
-    "net/http"
-    "path/filepath"
-    "fmt"
-    "log"
-    "os"
-    "os/exec"
-    "github.com/google/uuid"
-    "strings"
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/google/uuid"
 )
 
 var GIT_ROOT string = "git"
 
 func init() {
-    root, ok := os.LookupEnv("GITTO_ROOT")
-    if ok {
-        GIT_ROOT = root
-    }
-    var err error
-    GIT_ROOT, err = filepath.Abs(GIT_ROOT)
-    if err != nil {
-        log.Fatalf("Unable to resolve GITTO_ROOT: %s", err)
-    }
+	root, ok := os.LookupEnv("GITTO_ROOT")
+	if ok {
+		GIT_ROOT = root
+	}
+	var err error
+	GIT_ROOT, err = filepath.Abs(GIT_ROOT)
+	if err != nil {
+		log.Fatalf("Unable to resolve GITTO_ROOT: %s", err)
+	}
 }
 
 // The repo will be at {Root}/{Id}/{Name}.git
 type GitRepo struct {
-    Root string `json:"-"`
-    Id string `json:"id"`
-    Name string `json:"name"`
-    GitURL string `json:"git_url"`
+	Root   string `json:"-"`
+	Id     string `json:"id"`
+	Name   string `json:"name"`
+	GitURL string `json:"git_url"`
 }
 
 func (repo *GitRepo) GetPath() string {
-    name := repo.Name + ".git"
-    return filepath.Join(repo.Root, repo.Id, name)
+	name := repo.Name + ".git"
+	return filepath.Join(repo.Root, repo.Id, name)
 }
 
 // Initialize the Git URL using the base URL from the http Request
 func (repo *GitRepo) InitGitURL(r *http.Request) {
-    scheme := "https"
-    host := r.Host
+	scheme := "https"
+	host := r.Host
 
-    // Hack to make it work for localhost
-    if strings.HasPrefix(host, "localhost") {
-        scheme = "http"
-    }
+	// Hack to make it work for localhost
+	if strings.HasPrefix(host, "localhost") {
+		scheme = "http"
+	}
 
-    repo.GitURL = fmt.Sprintf("%s://%s/%s/%s.git", scheme, host, repo.Id, repo.Name)
+	repo.GitURL = fmt.Sprintf("%s://%s/%s/%s.git", scheme, host, repo.Id, repo.Name)
 }
 
 func GetRepo(id string) *GitRepo {
-    path := filepath.Join(GIT_ROOT, id)
+	path := filepath.Join(GIT_ROOT, id)
 
-    info, err := os.Stat(path)
-    if err != nil || !info.IsDir() {
-        return nil
-    }
+	info, err := os.Stat(path)
+	if err != nil || !info.IsDir() {
+		return nil
+	}
 
-    matches, _ := filepath.Glob(path + "/*.git")
-    if len(matches) == 0 {
-        return nil
-    }
+	matches, _ := filepath.Glob(path + "/*.git")
+	if len(matches) == 0 {
+		return nil
+	}
 
-    name := filepath.Base(matches[0])
-    name = strings.Split(name, ".")[0]
+	name := filepath.Base(matches[0])
+	name = strings.Split(name, ".")[0]
 
-    return &GitRepo{
-        Root: GIT_ROOT,
-        Id: id,
-        Name: name,
-    }
+	return &GitRepo{
+		Root: GIT_ROOT,
+		Id:   id,
+		Name: name,
+	}
 }
 
 func RepoFromPath(path string) *GitRepo {
-    // TODO: don't ignore errors
-    if !strings.HasSuffix(path, ".git") {
-        return nil
-    }
-    parent := filepath.Dir(path)
-    root := filepath.Dir(parent)
+	// TODO: don't ignore errors
+	if !strings.HasSuffix(path, ".git") {
+		return nil
+	}
+	parent := filepath.Dir(path)
+	root := filepath.Dir(parent)
 
-    id := filepath.Base(parent)
-    name := strings.Replace(filepath.Base(path), ".git", "", -1)
+	id := filepath.Base(parent)
+	name := strings.Replace(filepath.Base(path), ".git", "", -1)
 
-    return &GitRepo{
-        Root: root,
-        Id: id,
-        Name: name,
-    }
+	return &GitRepo{
+		Root: root,
+		Id:   id,
+		Name: name,
+	}
 }
 
 func NewRepo(name string) (GitRepo, error) {
-    uuid_, err := uuid.NewRandom()
+	uuid_, err := uuid.NewRandom()
 
-    // convert uuid to string and remove hyphens
-    id := strings.Replace(uuid_.String(), "-", "", -1)
+	// convert uuid to string and remove hyphens
+	id := strings.Replace(uuid_.String(), "-", "", -1)
 
-    if err != nil {
-        return GitRepo{}, err
-    }
+	if err != nil {
+		return GitRepo{}, err
+	}
 
-    repo := GitRepo{
-        Root: GIT_ROOT,
-        Id: id,
-        Name: name,
-    }
+	repo := GitRepo{
+		Root: GIT_ROOT,
+		Id:   id,
+		Name: name,
+	}
 
-    err = repo.initRepo()
-    if err != nil {
-        msg := fmt.Sprintf("Failed to create repository (%s)", err)
-        return GitRepo{}, errors.New(msg)
-    }
+	err = repo.initRepo()
+	if err != nil {
+		msg := fmt.Sprintf("Failed to create repository (%s)", err)
+		return GitRepo{}, errors.New(msg)
+	}
 
-    err = repo.installPostReceive()
-    if err != nil {
-        msg := fmt.Sprintf("Failed to create repository (%s)", err)
-        return GitRepo{}, errors.New(msg)
-    }
+	err = repo.installPostReceive()
+	if err != nil {
+		msg := fmt.Sprintf("Failed to create repository (%s)", err)
+		return GitRepo{}, errors.New(msg)
+	}
 
-    log.Println("Created new repo at", repo.GetPath())
-    return repo, nil
+	log.Println("Created new repo at", repo.GetPath())
+	return repo, nil
 }
 
 func (repo *GitRepo) GetCommitHash() string {
-    path := repo.GetPath()
-    cmd := exec.Command(
-            "git", "-C", path,
-            "rev-parse", "HEAD")
-    out, err := cmd.Output()
-    if err != nil {
-        log.Printf("%s: failed to get commit hash (%s)\n", repo.Id, err)
-        return ""
-    }
-    return strings.TrimSpace(string(out))
+	path := repo.GetPath()
+	cmd := exec.Command(
+		"git", "-C", path,
+		"rev-parse", "HEAD")
+	out, err := cmd.Output()
+	if err != nil {
+		log.Printf("%s: failed to get commit hash (%s)\n", repo.Id, err)
+		return ""
+	}
+	return strings.TrimSpace(string(out))
 }
 
 // initializes the repo by invoking git init
 func (repo *GitRepo) initRepo() error {
-    path := repo.GetPath()
-    cmd := exec.Command(
-            "git", "init",
-            "--bare",
-            "--initial-branch", "main",
-            path)
-    err := cmd.Run()
-    if err != nil {
-        log.Printf("%s: failed to initialize git repo (%s)\n", repo.Id, err)
-        return err
-    }
-    return nil
+	path := repo.GetPath()
+	cmd := exec.Command(
+		"git", "init",
+		"--bare",
+		"--initial-branch", "main",
+		path)
+	err := cmd.Run()
+	if err != nil {
+		log.Printf("%s: failed to initialize git repo (%s)\n", repo.Id, err)
+		return err
+	}
+	return nil
 }
 
 // install post-receive hook
 func (repo *GitRepo) installPostReceive() error {
-    hook_path := filepath.Join(repo.GetPath(), "hooks", "post-receive")
-    gitto_path, err := filepath.Abs(os.Args[0])
-    if err != nil {
-        log.Printf("%s: failed to install post-receive hook (%s)\n", repo.Id, err)
-        return err
-    }
+	hook_path := filepath.Join(repo.GetPath(), "hooks", "post-receive")
+	gitto_path, err := filepath.Abs(os.Args[0])
+	if err != nil {
+		log.Printf("%s: failed to install post-receive hook (%s)\n", repo.Id, err)
+		return err
+	}
 
-    err = os.Symlink(gitto_path, hook_path)
-    if err != nil{
-        log.Printf("%s: failed to install post-receive hook (%s)\n", repo.Id, err)
-        return err
-    }
-    return nil
+	err = os.Symlink(gitto_path, hook_path)
+	if err != nil {
+		log.Printf("%s: failed to install post-receive hook (%s)\n", repo.Id, err)
+		return err
+	}
+	return nil
 }
 
 // Returns the Webhook URL if exists, or empty string
 func (repo *GitRepo) GetWebhookURL() string {
-    path := filepath.Join(repo.GetPath(), "hooks", "webhook.txt")
+	path := filepath.Join(repo.GetPath(), "hooks", "webhook.txt")
 
-    content, err := os.ReadFile(path)
-    if err != nil {
-        return ""
-    }
-    url := string(content)
-    return strings.TrimSpace(url)
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return ""
+	}
+	url := string(content)
+	return strings.TrimSpace(url)
 }
 
 // Returns the Webhook URL if exists, or empty string
 func (repo *GitRepo) SetWebhookURL(url string) error {
-    path := filepath.Join(repo.GetPath(), "hooks", "webhook.txt")
-    return os.WriteFile(path, []byte(url), 0755)
+	path := filepath.Join(repo.GetPath(), "hooks", "webhook.txt")
+	return os.WriteFile(path, []byte(url), 0755)
 }
 
 func (repo *GitRepo) TriggerWebhook() {
-    // fmt.Println("Triggering webhook", repo.GetWebhookURL())
+	// fmt.Println("Triggering webhook", repo.GetWebhookURL())
 
-    url := repo.GetWebhookURL()
-    if url == "" {
-        return
-    }
+	url := repo.GetWebhookURL()
+	if url == "" {
+		return
+	}
 
-    values := map[string]string{
-        "repo_id": repo.Id,
-        "repo_name": repo.Name,
-        "git_commit_hash": repo.GetCommitHash(),
-    }
-    json_data, _ := json.Marshal(values)
+	values := map[string]string{
+		"repo_id":         repo.Id,
+		"repo_name":       repo.Name,
+		"git_commit_hash": repo.GetCommitHash(),
+	}
+	json_data, _ := json.Marshal(values)
 
-    resp, err := http.Post(url, "application/json", bytes.NewBuffer(json_data))
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(json_data))
 
-    if err != nil {
-        log.Fatal(err)
-    }
-    io.Copy(os.Stdout, resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	io.Copy(os.Stdout, resp.Body)
 }
--- server.go
+++ server.go
@@ -4,162 +4,167 @@
 package main
 
 import (
-    "encoding/json"
-    "net/http/cgi"
-    "net/http"
-    "fmt"
-    "regexp"
-    "log"
-    "strings"
-    "os"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/cgi"
+	"os"
+	"regexp"
+	"strings"
 )
 
 type NewRepoRequest struct {
-    Name string `json:"name"`
+	Name string `json:"name"`
 }
 
 type Webhook struct {
-    URL string `json:"url"`
+	URL string `json:"url"`
+}
+
+type WebhookRequest struct {
+	GitURL        string `json:"git_url"`
+	GitCommitHash string `json:"git_commit_hash"`
 }
 
 func Serve() {
-    http.HandleFunc("/api/", handleAPI)
-    http.HandleFunc("/", gitHttpBackend)
+	http.HandleFunc("/api/", handleAPI)
+	http.HandleFunc("/", gitHttpBackend)
 
-    fmt.Println("http://localhost:8080/")
-    http.ListenAndServe(":8080", nil)
+	fmt.Println("http://localhost:8080/")
+	http.ListenAndServe(":8080", nil)
 }
 
 var REGEX_REPO = regexp.MustCompile("^/api/repos/([0-9a-f]+)$")
 var REGEX_HOOK = regexp.MustCompile("^/api/repos/([0-9a-f]+)/hook$")
 
 func handleAPI(w http.ResponseWriter, r *http.Request) {
-    path := r.URL.Path
+	path := r.URL.Path
 
-    if path == "/api/repos" {
-        apiCreateRepo(w, r)
-        return
-    }
+	if path == "/api/repos" {
+		apiCreateRepo(w, r)
+		return
+	}
 
-    matches := REGEX_REPO.FindStringSubmatch(path)
-    if matches != nil {
-        apiRepo(w, r, matches[1])
-        return
-    }
+	matches := REGEX_REPO.FindStringSubmatch(path)
+	if matches != nil {
+		apiRepo(w, r, matches[1])
+		return
+	}
 
-    matches = REGEX_HOOK.FindStringSubmatch(path)
-    if matches != nil {
-        apiRepoHook(w, r, matches[1])
-        return
-    }
+	matches = REGEX_HOOK.FindStringSubmatch(path)
+	if matches != nil {
+		apiRepoHook(w, r, matches[1])
+		return
+	}
 
-    w.WriteHeader(404)
+	w.WriteHeader(404)
 }
 
 func apiCreateRepo(w http.ResponseWriter, r *http.Request) {
-    log.Printf("Req: %s %s\n", r.Host, r.URL.Path)
+	log.Printf("Req: %s %s\n", r.Host, r.URL.Path)
 
-    if r.Method != "POST" {
-        w.WriteHeader(405)
-        return
-    }
+	if r.Method != "POST" {
+		w.WriteHeader(405)
+		return
+	}
 
-    var req NewRepoRequest
+	var req NewRepoRequest
 
-    err := json.NewDecoder(r.Body).Decode(&req)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusBadRequest)
-        return
-    }
-    repo, err := NewRepo(req.Name)
-    if err != nil {
-        http.Error(w, "Failed to create repo", 500)
-    }
-    repo.InitGitURL(r)
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	repo, err := NewRepo(req.Name)
+	if err != nil {
+		http.Error(w, "Failed to create repo", 500)
+	}
+	repo.InitGitURL(r)
 
-    body, _ := json.Marshal(repo)
-    w.Write(body)
+	body, _ := json.Marshal(repo)
+	w.Write(body)
 }
 
 func apiRepo(w http.ResponseWriter, r *http.Request, repo_id string) {
-    repo := GetRepo(repo_id)
+	repo := GetRepo(repo_id)
 
-    if repo == nil {
-        w.WriteHeader(404)
-        return
-    }
-    repo.InitGitURL(r)
+	if repo == nil {
+		w.WriteHeader(404)
+		return
+	}
+	repo.InitGitURL(r)
 
-    body, _ := json.Marshal(repo)
-    w.Write(body)
+	body, _ := json.Marshal(repo)
+	w.Write(body)
 }
 
 func apiRepoHook(w http.ResponseWriter, r *http.Request, repo_id string) {
-    repo := GetRepo(repo_id)
+	repo := GetRepo(repo_id)
 
-    if repo == nil {
-        w.WriteHeader(404)
-        return
-    }
+	if repo == nil {
+		w.WriteHeader(404)
+		return
+	}
 
-    if r.Method == "GET" {
-        hook := Webhook{
-            URL: repo.GetWebhookURL(),
-        }
-        w.Header().Set("Content-type", "application/json")
-        body, _ := json.Marshal(hook)
-        w.Write(body)
-        return
-    } else if r.Method == "POST" {
-        var hook Webhook
+	if r.Method == "GET" {
+		hook := Webhook{
+			URL: repo.GetWebhookURL(),
+		}
+		w.Header().Set("Content-type", "application/json")
+		body, _ := json.Marshal(hook)
+		w.Write(body)
+		return
+	} else if r.Method == "POST" {
+		var hook Webhook
 
-        err := json.NewDecoder(r.Body).Decode(&hook)
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusBadRequest)
-            return
-        }
+		err := json.NewDecoder(r.Body).Decode(&hook)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
 
-        repo.SetWebhookURL(hook.URL)
+		repo.SetWebhookURL(hook.URL)
 
-        w.Header().Set("Content-type", "application/json")
-        body, _ := json.Marshal(hook)
-        w.Write(body)
-        return
-    }
+		w.Header().Set("Content-type", "application/json")
+		body, _ := json.Marshal(hook)
+		w.Write(body)
+		return
+	}
 }
 
 func gitHttpBackend(w http.ResponseWriter, r *http.Request) {
-    handler := cgi.Handler{
-        Path: "/usr/bin/git",
-        Args: []string{"http-backend"},
-        Env: []string {
-            "GIT_PROJECT_ROOT=" + GIT_ROOT,
-            "GIT_HTTP_EXPORT_ALL=",
-            "REMOTE_USER=git",
-        },
-    }
-    handler.ServeHTTP(w, r)
+	handler := cgi.Handler{
+		Path: "/usr/bin/git",
+		Args: []string{"http-backend"},
+		Env: []string{
+			"GIT_PROJECT_ROOT=" + GIT_ROOT,
+			"GIT_HTTP_EXPORT_ALL=",
+			"REMOTE_USER=git",
+		},
+	}
+	handler.ServeHTTP(w, r)
 }
 
 // tests if this command has been called from post-receive hook
 func isPostReceive() bool {
-    return strings.HasSuffix(os.Args[0], "post-receive")
+	return strings.HasSuffix(os.Args[0], "post-receive")
 }
 
 func handlePostReceive() {
-    pwd, _ := os.Getwd()
-    repo := RepoFromPath(pwd)
-    if repo != nil {
-        repo.TriggerWebhook()
-    } else {
-        fmt.Printf("Failed to find the repo: %s", pwd)
-    }
+	pwd, _ := os.Getwd()
+	repo := RepoFromPath(pwd)
+	if repo != nil {
+		repo.TriggerWebhook()
+	} else {
+		fmt.Printf("Failed to find the repo: %s", pwd)
+	}
 }
 
 func main() {
-    if isPostReceive() {
-        handlePostReceive()
-    } else {
-        Serve()
-    }
+	if isPostReceive() {
+		handlePostReceive()
+	} else {
+		Serve()
+	}
 }
